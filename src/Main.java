5d1fc8e0dc69e77fed9de8f7e3522f5ca973cfb9
Kevin Desousa <kevin.desousa1@uoit.net>
2018-03-26 14:32:29 -0400

    Added a list view, and some code to display images with it

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -27,6 +30,8 @@ public class Main extends Application {
     private Menu loginItem;
     private Menu searchItem;
     private Menu quitItem;
+    private ListView<String> videoList;
+
 
     public void start(Stage primaryStage) {
         centerPane = new BorderPane();
@@ -58,7 +63,33 @@ public class Main extends Application {
 
         menuBar.getMenus().addAll(loginItem, searchItem, quitItem);
         mainPane.setBackground(new Background(new BackgroundFill(Color.web("#F1F1F1"), CornerRadii.EMPTY, Insets.EMPTY)));
-        
+
+
+        videoList = new ListView<>();
+        videoList.setItems(FXCollections.observableArrayList());
+       /* videoList.setCellFactory(param -> new ListCell<String>() { //This will be needed to put videos onto the list
+            private ImageView imageView = new ImageView();
+            public void updateItem(String name, boolean empty) {
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    if(name.equals("RUBY"))
+                        imageView.setImage(IMAGE_RUBY);
+                    else if(name.equals("APPLE"))
+                        imageView.setImage(IMAGE_APPLE);
+                    else if(name.equals("VISTA"))
+                        imageView.setImage(IMAGE_VISTA);
+                    else if(name.equals("TWITTER"))
+                        imageView.setImage(IMAGE_TWITTER);
+                    setText(name);
+                    setGraphic(imageView);
+                }
+            }
+        });*/
+        centerPane.setLeft(videoList);
+
+
         mainPane.setTop(menuBar);
         mainPane.setCenter(centerPane);
         mainScene = new Scene(mainPane,500,200);
