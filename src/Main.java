68bf1c91b8f1f170d55f8d9a0a5a4e1843f0f17d
DanBullockCS <Daniel.Bullock@uoit.net>
2018-03-27 23:58:07 -0400

    Fixed aspect ratios on windows and added images folder as opposed to getting image via url

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,58 @@
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-
-import java.awt.event.KeyEvent;
-import java.util.concurrent.TimeUnit;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
-    public static void main(String[] args) { launch(args); }
-
     private Scene mainScene;
-    private BorderPane loadingPane;
     private BorderPane mainPane;
     private BorderPane centerPane;
-
     private TextField searchField;
     private MenuBar menuBar;
-    private Menu loginItem;
     private Menu searchItem;
     private Menu quitItem;
-    private Image ytlogo;
-    private ImageView imageLogoView;
-    private ListView<String> videoList;
+    private ListView<Video> videoList;
+    private WebView webview;
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     public void start(Stage primaryStage) {
-        loadingPane = new BorderPane();
         centerPane = new BorderPane();
         mainPane = new BorderPane();
-        searchField = new TextField("Enter your search query here");
+        searchField = new TextField();
+        webview = new WebView();
+        searchField.setPromptText("Enter your search query here");
         menuBar = new MenuBar();
-        loginItem = new Menu("Login");
         searchItem = new Menu("Search");
         quitItem = new Menu("Quit");
 
-        // Loading screen code...................
-        ytlogo = new Image("https://www.youtube.com/yt/about/media/images/brand-resources/icons/YouTube-icon-our_icon.png");
-        imageLogoView = new ImageView();
-        imageLogoView.setImage(ytlogo);
-
-        loadingPane.setCenter(imageLogoView);
-
-        mainScene = new Scene(loadingPane,500,200);
-        primaryStage.setScene(mainScene);
-        primaryStage.show();
-
-        // Times out code for 2 seconds to show logo (loading screen)
-        try {
-            TimeUnit.SECONDS.sleep(2);
-            loadingPane.getChildren().clear();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        // End of loading screen code...................
-        
         // Allows the menu to have just one item and also fire
         searchItem.getItems().add(new MenuItem());
         searchItem.addEventHandler(Menu.ON_SHOWN, event -> searchItem.hide());
         searchItem.addEventHandler(Menu.ON_SHOWING, event -> searchItem.fire());
-        searchItem.setOnAction(e -> centerPane.setTop(searchField));
+        searchItem.setOnAction(e -> {
+            if (centerPane.getTop() == searchField) {
+                centerPane.getChildren().remove(searchField);
+            } else {
+                centerPane.setTop(searchField);
+            }
+        });
 
         quitItem.getItems().add(new MenuItem());
         quitItem.addEventHandler(Menu.ON_SHOWN, event -> quitItem.hide());
@@ -77,45 +60,68 @@ public class Main extends Application {
         quitItem.setOnAction(e -> System.exit(0));
 
 
-        //Code for the search box to display or hide properly
+        // Code for the search box to display or hide properly
         searchField.setOnKeyPressed((event) -> {
-            if(event.getCode() == KeyCode.ESCAPE){
+            if (event.getCode() == KeyCode.ESCAPE || event.getCode() == KeyCode.ENTER) {
+                if (!searchField.getText().equals("") && event.getCode() == KeyCode.ENTER) {
+                    videoList.setItems(FXCollections.observableList(VideoRetriever.getVideos(searchField.getText())));
+                }
                 centerPane.getChildren().remove(searchField);
             }
         });
 
-        menuBar.getMenus().addAll(loginItem, searchItem, quitItem);
+        menuBar.getMenus().addAll(searchItem, quitItem);
         mainPane.setBackground(new Background(new BackgroundFill(Color.web("#F1F1F1"), CornerRadii.EMPTY, Insets.EMPTY)));
-
 
         videoList = new ListView<>();
         videoList.setItems(FXCollections.observableArrayList());
-       /* videoList.setCellFactory(param -> new ListCell<String>() { //This will be needed to put videos onto the list
+        // Placing videos onto ListView
+        videoList.setCellFactory(param -> new ListCell<Video>() {
             private ImageView imageView = new ImageView();
-            public void updateItem(String name, boolean empty) {
+            public void updateItem(Video video, boolean empty) {
+                super.updateItem(video, empty);
                 if (empty) {
                     setText(null);
                     setGraphic(null);
                 } else {
-                    if(name.equals("RUBY"))
-                        imageView.setImage(IMAGE_RUBY);
-                    else if(name.equals("APPLE"))
-                        imageView.setImage(IMAGE_APPLE);
-                    else if(name.equals("VISTA"))
-                        imageView.setImage(IMAGE_VISTA);
-                    else if(name.equals("TWITTER"))
-                        imageView.setImage(IMAGE_TWITTER);
-                    setText(name);
+                    imageView.setImage(new Image(video.getThumbnailURL()));
+                    setText(video.getTitle());
                     setGraphic(imageView);
                 }
             }
-        });*/
+
+        });
+        videoList.setOnMouseClicked(e -> {
+            System.out.println(videoList.getSelectionModel().getSelectedItem());
+            if (videoList.getSelectionModel().getSelectedItem() != null) {
+                System.out.println(videoList.getSelectionModel().getSelectedItem().getVideoURL());
+                webview.getEngine().load(
+                        videoList.getSelectionModel().getSelectedItem().getVideoURL()
+                );
+            }
+        });
+        
         centerPane.setLeft(videoList);
+        centerPane.setRight(webview);
 
         mainPane.setTop(menuBar);
         mainPane.setCenter(centerPane);
-        mainScene = new Scene(mainPane,500,200);
-        primaryStage.setScene(mainScene);
-        primaryStage.show();
+        mainScene = new Scene(mainPane, 1050, 500);
+
+        // Creates and starts the logo display
+        Stage logoStage = new Stage();
+        new LogoScreen().start(logoStage);
+
+        // Waits then displays the main screen
+        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+        pause.setOnFinished(e -> {
+            primaryStage.setResizable(false);
+            logoStage.close();
+            primaryStage.setScene(mainScene);
+            primaryStage.getIcons().add(new Image("file:images/icon.png"));
+            primaryStage.setTitle("YT Companion");
+            primaryStage.show();
+        });
+        pause.play();
     }
 }
---
+++ src/Video.java
@@ -0,0 +1,25 @@
+public class Video {
+    private String title;
+    private String thumbnailURL;
+    private String videoID;
+
+    public Video(String title, String thumbnailURL, String videoID) {
+        this.title = title;
+        this.thumbnailURL = thumbnailURL;
+        this.videoID = videoID;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+    public String getThumbnailURL() {
+        return this.thumbnailURL;
+    }
+    public String getVideoID() {
+        return this.videoID;
+    }
+    public String getVideoURL(){
+        return ("https://www.youtube.com/embed/" + this.getVideoID() + "?autoplay=1");
+    }
+
+}
